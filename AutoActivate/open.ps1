77a396164475c96acd6f880787b62b3c6c9f90cb
TrmiDo <111090515+TrmiDo@users.noreply.github.com>
2023-07-29 23:21:20 -0400

    Update open.ps1

--- AutoActivate/open.ps1
+++ AutoActivate/open.ps1
@@ -1,1 +1,4 @@
 invoke-item "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
+invoke-item "C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE"
+invoke-item "C:\Program Files\Microsoft Office\root\Office16\WINWORD.EXE"
+invoke-item "D:\WireSHARK\Wireshark.exe"
